51b42971ff9eb437c61678d31e2963da8002d795
John Cairns <john@2ad.com>
2022-01-30 16:48:02 -0600

    add iberia shell

---
+++ iberia/iberia.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env /bin/sh
+
+WEST=-10
+EAST=8
+NORTH=46
+SOUTH=34
+
+WIDTH=15c
+PROJECTION=-JM10/${WIDTH}
+
+OPT="-V --FONT_ANNOT_PRIMARY=10p"
+
+PEN=0.25p,200/200/200
+LINE=-W${PEN}
+LAND=255
+LAKE=170
+RIVER=220
+TRANS=15
+MINAREA=-A100
+SCALEBAR="f-5.5/32/40/250M"
+
+if [ ! -x $(which gmt) ]
+then
+    echo GMT required
+    exit 1
+fi
+
+VERSION=$(gmt --version)
+
+if [ "${VERSION}" = [0-5]* ]
+then
+    gmt --version
+    echo gmt 6 reqired
+    exit 1
+fi
+
+gmt gtd2cpt --show-sharedir
+
+# ETOPO1_Bed_g_gmt4.grd is the NETCDF encoded ETOPO1 dataset downloaded for GMT4 Bedrock
+BEDROCK=ETOPO1_europe.grd
+
+if [ -f /bedrock/${BEDROCK} ]
+then
+    ETOPO1=/bedrock/${BEDROCK}
+else
+    ETOPO1=../ETOPO1_Bed_g_gmt4.grd
+fi
+
+if [ ! -f ${ETOPO1} ]
+then
+    echo "ETOPO1 GRID file is required"
+    exit 1
+fi
+
+gmt set PS_LINE_CAP=ROUND PS_LINE_JOIN=ROUND PS_SCALE_X=1 PS_SCALE_Y=1 MAP_ORIGIN_X=1c MAP_ORIGIN_Y=1c ANNOT_FONT_PRIMARY=18
+
+BASEMAP='-B10dg10d -B+gwhite'
+
+PROJECT='iberia'
+
+gmt begin /pdf/${PROJECT}
+    gmt basemap -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} ${BASEMAP} 
+    gmt makecpt -Cgrey -T-50/1500
+    gmt grdimage ${ETOPO1} -n+c -I+a45+nt1 ${PROJECTION} -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${OPT}
+
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH}  ${PROJECTION} ${OPT} -G${LAND}/${LAND}/${LAND}@${TRANS} ${MINAREA}
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -S${LAKE}/${LAKE}/${LAKE} ${MINAREA}
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} ${LINE} ${MINAREA}
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -I0/0.5p,${RIVER}/${RIVER}/${RIVER} -I1/0.5p,${RIVER}/${RIVER}/${RIVER} -I2/0.5p,${RIVER}/${RIVER}/${RIVER} ${MINAREA}
+    # water bounds
+    gmt coast -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -L${SCALEBAR} -N3/0.25p,${LAKE}/${LAKE}/${LAKE} ${MINAREA}
+
+    if [ -f city.dat ]
+    then
+        cat city.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f6p,Helvetica+jCB
+        cat city.dat | gmt plot -Sc2p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    fi
+
+    if [ -f castle.dat ]
+    then
+        cat castle.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f6p,ZapfChancery-MediumItalic+jCB
+        cat castle.dat | gmt plot -Sd4p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    fi
+
+    if [ -f monastery.dat ]
+    then
+        cat monastery.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f6p,Optima-Bold+jCB    
+        cat monastery.dat | gmt plot -Sh4p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    fi
+
+    if [ -f battle.dat ]
+    then
+        cat battle.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f6p,Times-Roman+jCB
+        cat battle.dat | gmt plot -S+6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT}
+    fi
+
+    if [ -f place.dat ]
+    then
+        cat place.dat | gmt text -Dj6p -R${WEST}/${EAST}/${SOUTH}/${NORTH} ${PROJECTION} ${OPT} -F+f6p,AvantGarde-Book+jCB
+    fi
+gmt end
